da77dc4e04ce75a0882744af29054572305f784a
Zineb.machrouh <zinebmac49@gmail.com>
2025-11-14 10:17:35 +0100

    initiate java project

---
+++ src/main/java/com/bloomberg/fxdeals/FxDealsDataWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.bloomberg.fxdeals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FxDealsDataWarehouseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FxDealsDataWarehouseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bloomberg/fxdeals/FxDealsDataWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bloomberg.fxdeals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FxDealsDataWarehouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
