53e54dd9206cb7896d0981a05a2e0c85d4ac0bb3
Zineb.machrouh <zinebmac49@gmail.com>
2025-11-15 22:15:03 +0100

    feat: add rest endpoint for batch deal import

--- src/main/java/com/bloomberg/fxdeals/dtos/res/FxDealResDTO.java
+++ src/main/java/com/bloomberg/fxdeals/dtos/res/FxDealResDTO.java
@@ -1,11 +1,11 @@
 package com.bloomberg.fxdeals.dtos.res;
 
-import java.math.BigInteger;
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
 public record FxDealResDTO(String dealId,
                            String fromCurrency,
                            String toCurrency,
                            LocalDateTime dealTimestamp,
-                           BigInteger dealAmount) {
+                           BigDecimal dealAmount) {
 }
