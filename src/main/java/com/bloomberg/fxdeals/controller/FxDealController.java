7bfb9c753bfa6437b820b1684d2c800522e7fca4
Zineb.machrouh <zinebmac49@gmail.com>
2025-11-16 05:39:41 +0100

    feat: add comprehensive logging

--- src/main/java/com/bloomberg/fxdeals/controller/FxDealController.java
+++ src/main/java/com/bloomberg/fxdeals/controller/FxDealController.java
@@ -4,11 +4,13 @@ import com.bloomberg.fxdeals.dtos.req.FxDealReqDTO;
 import com.bloomberg.fxdeals.dtos.res.FxDealResDTO;
 import com.bloomberg.fxdeals.service.FxDealService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/api/v1/deals")
@@ -18,18 +20,27 @@ public class FxDealController {
     @PostMapping("/import/single")
     @ResponseStatus(HttpStatus.CREATED)
     public FxDealResDTO importSingleDeal(@RequestBody FxDealReqDTO fxDealReq) {
-        return service.importSingleDeal(fxDealReq);
+        log.info("Received request to import single deal with ID: {}", fxDealReq.dealId());
+        FxDealResDTO result = service.importSingleDeal(fxDealReq);
+        log.info("Successfully imported deal with ID: {}", result.dealId());
+        return result;
     }
 
     @PostMapping("/import/batch")
     @ResponseStatus(HttpStatus.CREATED)
     public List<FxDealResDTO> importBatchDeals(@RequestBody List<FxDealReqDTO> fxDealReqs) {
-        return service.importBatchDeals(fxDealReqs);
+        log.info("Received request to import batch of {} deals", fxDealReqs.size());
+        List<FxDealResDTO> results = service.importBatchDeals(fxDealReqs);
+        log.info("Successfully imported {} deals in batch", results.size());
+        return results;
     }
 
 
     @GetMapping
     public List<FxDealResDTO> getAllDeals() {
-        return service.getAllDeals();
+        log.info("Received request to retrieve all deals");
+        List<FxDealResDTO> deals = service.getAllDeals();
+        log.info("Retrieved {} deals", deals.size());
+        return deals;
     }
 }
